719376a5a6714e0e6732d68467a4f99263aa5533
Samadari <archanasamadari@gmail.com>
2025-05-02 22:30:00 +0530

    update navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -50,7 +50,7 @@ function Navbar() {
                   <svg className="w-5 sm:w-6 h-5 sm:h-6 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
                   </svg>
-                  Favorites
+                  Favourite
                   {favorites.length > 0 && (
                     <span className="absolute -top-1 -right-1 bg-gradient-to-r from-blue-500 to-purple-500 text-white text-xs rounded-full h-4 sm:h-5 w-4 sm:w-5 flex items-center justify-center shadow-md">
                       {favorites.length}
